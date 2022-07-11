5e4e75205f80f801dba8be0ec9e685709e1f69e4
chess2022 <lakeshore.hnh@gmail.com>
2022-07-11 13:09:34 -0700

    adds CRUD routes and admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Finch(models.Model):
+    photo = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={finch_id: self.id})
