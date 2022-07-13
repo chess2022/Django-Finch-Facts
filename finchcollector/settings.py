d8fd2d85b1cebffe75713b6081c01be50a99a343
chess2022 <lakeshore.hnh@gmail.com>
2022-07-12 21:55:28 -0700

    Adds .env

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -19,8 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9tcp*atwif*645@#8d96mv947x_kky+y1d)*zl1s55e2=uzbsx'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
