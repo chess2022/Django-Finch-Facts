2fdb0c82b0922f86a69d7b0c3e0761a23743b9c6
chess2022 <lakeshore.hnh@gmail.com>
2022-07-13 23:22:19 -0700

    Adds sightings model and form

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
+SIGHTINGS = (
+    ('S', 'Physically Seen'),
+    ('H', 'Heard Song')
+)
 
 class Finch(models.Model):
     photo = models.CharField(max_length=200)
@@ -13,4 +18,19 @@ class Finch(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={finch_id: self.id})
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+class Sighting(models.Model):
+    date = models.DateField('sighting date')
+    type = models.CharField(
+        max_length=1,
+        choices=SIGHTINGS,
+        default=SIGHTINGS[0][0]
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_sighting_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
