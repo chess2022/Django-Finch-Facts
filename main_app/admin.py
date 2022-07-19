f825654ee89ecac2c90b45449720796193f2124e
chess2022 <lakeshore.hnh@gmail.com>
2022-07-18 17:35:10 -0700

    Registers new models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Finch, Sighting
+from .models import Finch, Sighting, Region, Photo
 
 # Register your models here.
 admin.site.register(Finch)
 admin.site.register(Sighting)
+admin.site.register(Region)
+admin.site.register(Photo)
